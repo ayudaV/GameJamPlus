c4782ec8afdb413776d3fc4e3e2ab26b212a8620
André Yuhji Terada <andre.terada@ifood.com.br>
2024-10-19 15:44:14 -0300

    Init project

---
+++ gamejamplus/objects/train/scripts/train.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@export var animated_sprite : AnimatedSprite2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+		animated_sprite.play("walk")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite.play("default")
+
+	move_and_slide()
